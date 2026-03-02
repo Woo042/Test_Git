49062562a03c944112f4a68141ca9294ca7e163f
user <user@DESKTOP-WOOJIN>
2026-03-02 15:47:58 +0900

    초기 커밋.

---
+++ Test_Git/TestFunc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Git
+{
+    internal class TestFunc
+    {
+        public static void OutPut_Hi()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
